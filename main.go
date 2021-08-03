42ce8ddd71ba07b6a0d7efeccbacc79a44123c26
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-03 13:29:07 +0800

    Refactor: initialise fiber and middlewares

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	app := fiber.New()
+	
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	app.Get("/", hello)
+
+	app.Listen(":1337")
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello World")
 }
