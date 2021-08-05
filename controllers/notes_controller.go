04b2a8681bbd57188bea84180512e2f2a0df7f7b
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-05 14:00:45 +0800

    refactor: add delete note controller

--- controllers/notes_controller.go
+++ controllers/notes_controller.go
@@ -38,6 +38,7 @@ func AddNote(ctx *fiber.Ctx) error {
 	})
 }
 
+// GET - retrieve a note
 func GetNote(ctx *fiber.Ctx) error {
 	var note models.Note
 
@@ -48,10 +49,42 @@ func GetNote(ctx *fiber.Ctx) error {
 		})
 	}
 
-	database.DB.Model(&models.Note{}).Where("id = ?", noteId).Find(&note)
+	// If the result (noteId != note.ID) does not exist, return server error code and message
+	if result := database.DB.Model(&models.Note{}).Where("id = ?", noteId).First(&note); result.Error != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Unable to retrieve the specified noteId due to server error",
+		})
+	}
 
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
 		"message": "success",
 		"data": note,
+	})
+}
+
+// DELETE - remove a single note based on note id
+func DeleteNote(ctx *fiber.Ctx) error {
+	var note models.Note
+
+	noteId, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
+	if err != nil {
+		return ctx.Status(442).JSON(fiber.Map{
+			"message": "Unable to process JSON request",
+		})
+	}
+
+	// Get the note to delete from our database
+	db := database.DB.Model(&models.Note{}).Where("id = ?", noteId).Find(&note)
+	// Delete the note from our database
+	db.Delete(&note)
+
+	if db.Error != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Unable to perform DELETE request due to server error",
+		})
+	}
+
+	return ctx.Status(fiber.StatusNoContent).JSON(fiber.Map{
+		"message": "Note successfully deleted",
 	})
 }
