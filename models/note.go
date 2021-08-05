e50a2d10d16547c653e66b62e82d8d31ecd93b74
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-05 15:28:58 +0800

    refactor: remove gorm.Model and clean up

--- models/note.go
+++ models/note.go
@@ -1,11 +1,13 @@
 package models
 
 import (
-	"gorm.io/gorm"
+	"time"
 )
 
 type Note struct {
-	gorm.Model
-	Title string `gorm:"not null;"`
-	Content string
+	ID uint64 `gorm:"primary_key;auto_increment;" json:"id"`
+	Title string `gorm:"not null;" json:"title"`
+	Content string `json:"content"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
