169dda9c04f09611ebe39e67beceea86efa0c35a
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-07 00:59:21 +0800

    feature: add login, current user and logout controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,32 +1,38 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
 	"github.com/mofodox/TILNotes/database"
 	"github.com/mofodox/TILNotes/models"
 	"golang.org/x/crypto/bcrypt"
+	"os"
+	"strconv"
+	"time"
 )
 
+// Register POST - register user
 func Register(ctx *fiber.Ctx) error {
-	var user models.User
+	var data map[string]string
 
-	if err := ctx.BodyParser(&user); err != nil {
+	if err := ctx.BodyParser(&data); err != nil {
 		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Unable to process JSON request",
 		})
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword(user.Password, bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data["password"]), bcrypt.DefaultCost)
 	if err != nil {
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Unable to hash the password due to server error",
 		})
 	}
 
-	user = models.User{
-		Email: user.Email,
-		FirstName: user.FirstName,
-		LastName: user.LastName,
+	user := models.User{
+		Email: data["email"],
+		FirstName: data["first_name"],
+		LastName: data["last_name"],
 		Password: hashedPassword,
 	}
 
@@ -35,5 +41,100 @@ func Register(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "User registered successfully",
 		"data": user,
+	})
+}
+
+// Login Post - login user
+func Login(ctx *fiber.Ctx) error {
+	var (
+		data map[string]string
+		user models.User
+	)
+
+	if err := ctx.BodyParser(&data); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Unable to process JSON request",
+		})
+	}
+
+	database.DB.Model(&user).Where("email = ?", data["email"]).First(&user)
+
+	if user.ID == 0 {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": fmt.Sprintf("User ID %v not found", user.ID),
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(os.Getenv("JWTSecret")))
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Unable to create token due to server error",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	ctx.Cookie(&cookie)
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message":   "Success",
+		"data":      user,
+	})
+}
+
+// CurrentUser GET - retrieve current logged in user
+func CurrentUser(ctx *fiber.Ctx) error {
+	cookie := ctx.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWTSecret")), nil
+	})
+	if err != nil || !token.Valid {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Success",
+		"data": user,
+	})
+}
+
+// Logout POST - logout user
+func Logout(ctx *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: "",
+		Expires: time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	ctx.Cookie(&cookie)
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Logout successfully",
 	})
 }
