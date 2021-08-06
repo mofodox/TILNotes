0ceffe2493d628e555c4297c633806f575dbc8a5
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-06 21:17:47 +0800

    refactor: update router add user auth routes

--- routes/router.go
+++ routes/router.go
@@ -8,6 +8,9 @@ import (
 func SetupRoutes(app *fiber.App) {
 	noteApiV1 := app.Group("/api/v1/notes")
 	categoryApiV1 := app.Group("/api/v1/categories")
+	userAuthApiV1 := app.Group("/api/v1/users/auth")
+
+	userAuthApiV1.Post("/", controllers.Register)
 
 	noteApiV1.Get("/", controllers.GetAllNotes)
 	noteApiV1.Post("/", controllers.AddNote)
