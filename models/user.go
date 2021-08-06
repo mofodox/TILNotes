910f054161f8a1978e5b725e76e8d3a25518d4d5
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-06 21:17:21 +0800

    chore: create user model

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type User struct {
+	ID uint `gorm:"primary_key;auto_increment;" json:"id"`
+	Email string `gorm:"not null;unique;" json:"email"`
+	FirstName string `gorm:"not null;" json:"first_name"`
+	LastName string `gorm:"not null;" json:"last_name"`
+	Password []byte `gorm:"not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
