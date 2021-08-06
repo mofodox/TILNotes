666b65591bfe517e4e6eca9cc1b5030a18d005d4
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-07 01:02:14 +0800

    feature: add user auth endpoints
    Endpoints: register, login, current_user, logout

--- routes/router.go
+++ routes/router.go
@@ -10,7 +10,10 @@ func SetupRoutes(app *fiber.App) {
 	categoryApiV1 := app.Group("/api/v1/categories")
 	userAuthApiV1 := app.Group("/api/v1/users/auth")
 
-	userAuthApiV1.Post("/", controllers.Register)
+	userAuthApiV1.Post("/register", controllers.Register)
+	userAuthApiV1.Post("/login", controllers.Login)
+	userAuthApiV1.Get("/current_user", controllers.CurrentUser)
+	userAuthApiV1.Post("/logout", controllers.Logout)
 
 	noteApiV1.Get("/", controllers.GetAllNotes)
 	noteApiV1.Post("/", controllers.AddNote)
