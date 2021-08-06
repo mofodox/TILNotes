8ea73c0e72dcc122e1cb07a332da6fee907abb43
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-06 10:32:57 +0800

    refactor: add belongs_to relationship to Note

--- models/note.go
+++ models/note.go
@@ -8,6 +8,8 @@ type Note struct {
 	ID uint64 `gorm:"primary_key;auto_increment;" json:"id"`
 	Title string `gorm:"not null;" json:"title"`
 	Content string `json:"content"`
+	CategoryId uint `json:"category_id"`
+	Category Category `gorm:"foreignKey:CategoryId" json:"category"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
