5bec9c9867ae87f81140fa0601800426095492e6
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-08-07 15:43:42 +0800

    chore: add jwt helper methods

---
+++ util/jwt.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"github.com/golang-jwt/jwt"
+	"os"
+	"time"
+)
+
+func GenerateJwt(issuer string) (string, error) {
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    issuer,
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
+	})
+
+	return claims.SignedString([]byte(os.Getenv("JWTSecret")))
+}
+
+func ParseJwt(cookie string) (string, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWTSecret")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return "", err
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	return claims.Issuer, nil
+}
